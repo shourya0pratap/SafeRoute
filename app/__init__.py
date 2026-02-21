fa79eeb9db0a40facf9f6d494a5f9f6490b599ee
sp_3141 <07shourya.pratap@gmail.com>
2026-02-21 22:27:31 +0530

    Updated init.py

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+# File: app/__init__.py
+from flask import Flask
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Import the routes so Flask knows what pages exist
+from app import routes
